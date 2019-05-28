b26408dda7bc284309de181530a9c4924e4c0082
Álvaro Lovera Almagro <loveraalvaro@gmail.com>
2019-05-28 13:06:08 +0200

    heroku deploy2

--- src/main/java/com/calavera/reparto/configuration/SwaggerConfiguration.java
+++ src/main/java/com/calavera/reparto/configuration/SwaggerConfiguration.java
@@ -24,7 +24,10 @@ public class SwaggerConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.calavera.reparto")).paths(PathSelectors.any()).build()
+                .apis(RequestHandlerSelectors.
+                        basePackage("com.calavera.reparto"))
+                .paths(PathSelectors.any())
+                .build()
                 .pathMapping("/");
     }
 }
--- src/main/java/com/calavera/reparto/controller/RepartidorController.java
+++ src/main/java/com/calavera/reparto/controller/RepartidorController.java
@@ -13,12 +13,16 @@ import com.calavera.reparto.model.Repartidor;
 import com.calavera.reparto.repositories.EnvioRepository;
 import com.calavera.reparto.repositories.RepartidorRepository;
 import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -36,7 +40,8 @@ public class RepartidorController {
         this.envioRepo = eRepository;
     }
 
-    @GetMapping("/repartidor")
+    @GetMapping(value="/repartidor", produces= MediaType.APPLICATION_JSON_VALUE, headers = "Accept=application/json")
+    //@RequestMapping(value = "/repartidor", method = RequestMethod.GET,produces={MediaType.APPLICATION_JSON_UTF8_VALUE},headers = "Accept=application/xml")
     Iterable<Repartidor> all() {
         return repartidorRepo.findAll();
     }
